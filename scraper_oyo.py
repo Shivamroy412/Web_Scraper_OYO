a5c4a92c24ea4bf362edfb752423bc6846937876
Shivam Roy <roy.shivam412@gmail.com>
2020-06-20 16:22:02 +0530

    Added functionality to check multiple hotel webpages

--- scraper_oyo.py
+++ scraper_oyo.py
@@ -4,23 +4,53 @@ import smtplib
 import time
 
 
-URL = "https://www.oyorooms.com/56085-spot-on-spot-on-37467-shivram-hotel" \
-      "-jabalpur/?checkin=19%2F06%2F2020&checkout=20%2F06%2F2020&rooms=1" \
-      "&guests=1&rooms_config=1-1_0&selected_rcid=272950"
+start_scraping()   # Calls the main start function
+
+# Caution: the loop runs indefintely unless stopped manually, because this is
+# how it was supposed to be
+
+
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) ' \
             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 ' \
                          'Safari/537.36'}
 
-# Gives infor about the browser, Get by seraching "my user agent" on your browser
+# Gives information about the browser, Get it by searching "my user agent" on
+# your browser
 
-def check_price():
+
+
+
+# This dictionary is used to store the hotel URLs and the trigger prices
+
+hotels_prices = {
+
+    'https://www.oyorooms.com/96616-oyo-rooms-oyo-70088-gurugram-residency'
+    '-gurgaon/?checkin=17%2F09%2F2020&checkout=18%2F09%2F2020&rooms=1&guests'\
+    '=2&rooms_config=1-2_0&selected_rcid=1' : 800 ,
+
+    "https://www.oyorooms.com/56085-spot-on-spot-on-37467-shivram-hotel" \
+      "-jabalpur/?checkin=19%2F07%2F2020&checkout=20%2F07%2F2020&rooms=1" \
+      "&guests=1&rooms_config=1-1_0&selected_rcid=272950" : 500
+}
+
+
+
+
+def start_scraping(hotels_prices = hotels_prices):
+    while(True):
+        for hotel_URL, price in hotels_prices.items():
+            check_price(hotel_URL, price)
+        time.sleep(36000)  # The default time loop is 10 hours
+
+
+
+
+def check_price(URL, floor_price):
     page = requests.get(URL, headers = headers)
 
     soup = BeautifulSoup(page.text, 'html.parser')
 
-
-                    #print(soup.prettify())
 
     hotel_name = soup.find('h1', class_ = "c-1wj1luj").get_text()
 
@@ -36,8 +66,10 @@ def check_price():
     print(body)
 
 
-    if price >= 500:
+    if price <= floor_price:
         send_mail(hotel_name, price_string, URL)
+
+
 
 
 def send_mail(hotel_name, price_string, URL):
@@ -74,11 +106,12 @@ def send_mail(hotel_name, price_string, URL):
                     to_addrs= [], msg = msg)
 
     print('Mail sent.')
+    print(msg)
     server.quit()
 
-while(True):
-    check_price()
-    time.sleep(60 * 60 * 10)
 
-# The default time loop is 10 hours
+
+
+
+
 
